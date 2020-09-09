ada17a80ac3e48bfdc7bc0dfe41b82c286a8ca19
moshi <moshiur@cefalo>
2020-09-10 01:00:23 +0600

    version comparison

---
+++ SeptemberChallenge/VersionComparison.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeptemberChallenge
+{
+    public class VersionComparison
+    {
+        public int CompareVersion(string version1, string version2)
+        {
+
+            var v1Parts = version1.Split('.').ToList();
+            var v2Parts = version2.Split('.').ToList();
+
+            var maxPart = Math.Max(v1Parts.Count(), v2Parts.Count());
+
+            for (int i = 0; i < maxPart; i++)
+            {
+                var v1 = (i >= v1Parts.Count) ? 0 : Convert.ToInt32(v1Parts[i]);
+                var v2 = (i >= v2Parts.Count) ? 0 : Convert.ToInt32(v2Parts[i]);
+                if (v1 > v2) return 1;
+                if (v1 < v2) return -1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ SeptemberChalllenge.Tests/VersionComparisonTests.cs
@@ -0,0 +1,46 @@
+﻿using SeptemberChallenge;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SeptemberChalllenge.Tests
+{
+    public class VersionComparisonTests
+    {
+        private readonly VersionComparison versionComparison;
+        public VersionComparisonTests()
+        {
+            versionComparison = new VersionComparison();
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+            var actual = versionComparison.CompareVersion("0.9.9.9.9.9.9.9.9.9.9.9.9","1.0");
+            Assert.Equal(-1,actual);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var actual = versionComparison.CompareVersion("0.9.9.9.9.9.9.9.9.9.9.9.9.9.9.9.9", "1.0");
+            Assert.Equal(-1, actual);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var actual = versionComparison.CompareVersion("1.0","1");
+            Assert.Equal(0, actual);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var actual = versionComparison.CompareVersion("1","0");
+            Assert.Equal(1, actual);
+        }
+    }
+}
